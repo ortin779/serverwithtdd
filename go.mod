0a938dbd04ac81ef87ff5c1868d233f8f68ccb69
Naresh <chakalinaresh1729@gmail.com>
2024-07-07 23:25:32 +0530

    add player server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ortin779/serverwithtdd
+
+go 1.22.5
---
+++ server_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGETPlayers(t *testing.T) {
+	t.Run("returns Peppers score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "20"
+
+		if got != want {
+			t.Errorf("expected %q, but got %q", want, got)
+		}
+	})
+}
